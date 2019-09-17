4169633878f84521f009853390d75817ab3996f8
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-09-16 22:57:19 -0700

    Remove unused code

--- log-capture/log4j2/appender/src/main/java/bugbash/Application.java
+++ log-capture/log4j2/appender/src/main/java/bugbash/Application.java
@@ -2,18 +2,11 @@ package bugbash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 }
--- web-filter/manual-registration/spring-boot-2.x-jar/src/main/java/bugbash/Application.java
+++ web-filter/manual-registration/spring-boot-2.x-jar/src/main/java/bugbash/Application.java
@@ -2,23 +2,16 @@ package bugbash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 
     @Bean
