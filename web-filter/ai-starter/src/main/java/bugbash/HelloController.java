04f64d99e3ee8787817bc5aadb5776e43273ca1b
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-12 21:39:24 -0700

    Add micrometer to ai starter app

--- web-filter/ai-starter/src/main/java/bugbash/HelloController.java
+++ web-filter/ai-starter/src/main/java/bugbash/HelloController.java
@@ -3,11 +3,17 @@ package bugbash;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
+
 @RestController
 public class HelloController {
 
+    private final Counter counter = Metrics.counter("bug_bash_counter");
+    
     @RequestMapping("/hello")
     public String index() {
+        counter.increment();
         return "Hello!";
     }
 
